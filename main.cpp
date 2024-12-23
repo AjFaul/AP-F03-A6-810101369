3d3e0bbd7fd1268b3efc120de9f32ba6fa15f31e
AjFaul <amirabasajilchi@gmail.com>
2024-12-23 20:06:45 +0000

    Add handle input

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 #include <sstream>
+#include <list>
+#include <map>
 
 
 using namespace std;
-
-
-
 
 vector<string>  split(string Line,char DELIMITER)
 {
@@ -17,4 +17,41 @@ vector<string>  split(string Line,char DELIMITER)
         inputSection.push_back(token);
     }
     return inputSection;
+}
+
+void handle_restaurants(){}
+
+void handle_districts(){}
+
+
+
+
+void handle_input_data(char* argv[])
+{
+    string name_file_restaurants=argv[1];
+    string name_file_districts=argv[2];
+    
+    ifstream file_restaurants(name_file_restaurants);
+    ifstream file_districts(name_file_districts);
+
+    handle_restaurants();
+    handle_districts();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(int argc , char* argv[])
+{
+
 }
